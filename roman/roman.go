224e536af213ae0b82684e88a25f22c755de1ec2
yc yap <yowchun93@gmail.com>
2019-09-23 11:39:13 +0800

    TDD: Roman

--- roman/roman.go
+++ roman/roman.go
@@ -98,6 +98,31 @@ var allRomanNumerals = romanNumerals{
 	{1, "I"},
 }
 
+func (r romanNumerals) ValueOf(symbols ...byte) int {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
+func (r romanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
+func isSubtractive(symbol uint8) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
@@ -110,22 +135,26 @@ func ConvertToRoman(arabic int) string {
 	return result.String()
 }
 
-func (r romanNumerals) ValueOf(symbols ...byte) int {
-	symbol := string(symbols)
-	for _, s := range r {
-		if s.Symbol == symbol {
-			return s.Value
+type windowedRoman string
+
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
+
+		if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{byte(symbol), byte(w[i+1])})
+			i++
+		} else {
+			symbols = append(symbols, []byte{byte(symbol)})
 		}
 	}
-
-	return 0
+	return
 }
 
-// func ConvertToArabic(roman string) int {
-// 	total := 0
-// 	for range roman {
-// 		total++
-// 	}
-
-// 	return total
-// }
+func ConvertToArabic(roman string) (total int) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomanNumerals.ValueOf(symbols...)
+	}
+	return total
+}
--- roman/roman_test.go
+++ roman/roman_test.go
@@ -52,37 +52,31 @@ func TestRomanNumerals(t *testing.T) {
 }
 
 func TestArabicNumerals(t *testing.T) {
-	got := ConvertToArabic("I")
-	want := 1
-
-	if got != want {
-		t.Errorf("got %d, want %d", got, want)
+	cases := []struct {
+		Description string
+		Want        int
+		Roman       string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to I", 2, "II"},
+		{"3 gets converted to I", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"8 gets converted to IX", 8, "VIII"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"40 gets converted to XL", 40, "XL"},
 	}
 
-	t.Run("Test II returns 2", func(t *testing.T) {
-		got := ConvertToArabic("II")
-		want := 2
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			want := test.Want
 
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
-	})
-
-	t.Run("Test III returns 3", func(t *testing.T) {
-		got := ConvertToArabic("III")
-		want := 3
-
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
-	})
-
-	t.Run("Test IV returns 4", func(t *testing.T) {
-		got := ConvertToArabic("IV")
-		want := 4
-
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
-	})
+			if got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		})
+	}
 }
