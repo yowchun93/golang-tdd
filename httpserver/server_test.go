cbf5fcc13f246dfb5f3609cca0dd611d7d910d27
yc yap <yowchun93@gmail.com>
2019-11-04 09:36:30 +0800

    refactor

--- httpserver/server_test.go
+++ httpserver/server_test.go
@@ -68,7 +68,8 @@ func TestStoreWins(t *testing.T) {
 	server := &PlayerServer{&store}
 
 	t.Run("it returns accepted on POST", func(t *testing.T) {
-		request := newPostWinRequest("Pepper")
+		player := "Pepper"
+		request := newPostWinRequest(player)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
@@ -77,6 +78,10 @@ func TestStoreWins(t *testing.T) {
 
 		if len(store.winCalls) != 1 {
 			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
 		}
 	})
 }
