7b9640673c8cadc88e11b40c82dd66abcd98e932
yowchun93 <yowchun93@gmail.com>
2020-10-10 10:58:42 +0800

    Struct & Interfaces

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -20,22 +20,37 @@ type Shape interface {
 	Area() float64
 }
 
-func Perimeter(r Rectangle) float64 {
-	return (2 * (r.Width + r.Height))
+// Perimeter returns a perimeter for a shape
+// func Perimeter(width float64, height float64) float64 {
+// 	return (2 * width) + (2 * height)
+// }
+
+// // Area returns area for a shape
+// func Area(width float64, height float64) float64 {
+// 	return width * height
+// }
+
+// Version 2
+// This would not work if there is another type Shape
+// Perimeter returns a perimeter for a rectangle
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+// // Area returns a area for a rectangle
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
+
+// Version 3
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 func (c Circle) Area() float64 {
-	return (math.Pi * (c.Radius * c.Radius))
-}
-
-func (r Rectangle) Area() float64 {
-	return (r.Width * r.Height)
+	return math.Pi * c.Radius * c.Radius
 }
 
 func (t Triangle) Area() float64 {
 	return (t.Base * t.Height) * 0.5
-}
-
-func Area(c Circle) float64 {
-	return (math.Pi * (c.Radius * c.Radius))
 }
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -2,45 +2,66 @@ package shapes
 
 import "testing"
 
+// Version 1
+// func TestPerimeter(t *testing.T) {
+// 	got := Perimeter(10.0, 10.0)
+// 	want := 40.0
+
+// 	if got != want {
+// 		t.Errorf("got %.2f want %.2f", got, want)
+// 	}
+// }
+
+// func TestArea(t *testing.T) {
+// 	got := Area(12.0, 6.0)
+// 	want := 72.0
+
+// 	if got != want {
+// 		t.Errorf("got %.2f want %.2f", got, want)
+// 	}
+// }
+
+// Perimeter and Area using structs
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	want := 40.0
 	got := Perimeter(rectangle)
-	if want != got {
+	want := 40.0
+
+	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
 
-// Version 1
+// Version 2
 // func TestArea(t *testing.T) {
-
-// 	t.Run("circle", func(t *testing.T) {
-// 		circle := Circle{10}
-// 		want := 314.1592653589793
-// 		got := circle.Area()
-// 		if want != got {
-// 			t.Errorf("got %.2f want %.2f", got, want)
-// 		}
-// 	})
-
 // 	t.Run("rectangle", func(t *testing.T) {
 // 		rectangle := Rectangle{10.0, 10.0}
 // 		got := rectangle.Area()
 // 		want := 100.0
-// 		if want != got {
+
+// 		if got != want {
 // 			t.Errorf("got %.2f want %.2f", got, want)
+// 		}
+// 	})
+
+// 	t.Run("circles", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		got := circle.Area()
+// 		want := 314.1592653589793
+
+// 		if got != want {
+// 			t.Errorf("got %g want %g", got, want)
 // 		}
 // 	})
 // }
 
-// Version 2
+// Version 3
 // func TestArea(t *testing.T) {
-// 	// creating Helper test function
 // 	checkArea := func(t *testing.T, shape Shape, want float64) {
 // 		t.Helper()
 // 		got := shape.Area()
 // 		if got != want {
-// 			t.Errorf("got %.2f want %.2f", got, want)
+// 			t.Errorf("got %g want %g", got, want)
 // 		}
 // 	}
 
@@ -55,24 +76,22 @@ func TestPerimeter(t *testing.T) {
 // 	})
 // }
 
-// Version 3
-// refactored using Table driven tests
+// Table driven test
 func TestArea(t *testing.T) {
+
 	areaTests := []struct {
 		shape Shape
 		want  float64
 	}{
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
-		{Triangle{10, 10}, 50.0},
+		{Triangle{12, 6}, 36.0},
 	}
 
-	// this is beautiful, i know for a fact what i am iterating through
-	for _, test := range areaTests {
-		got := test.shape.Area()
-		want := test.want
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
 }
