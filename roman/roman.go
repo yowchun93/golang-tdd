25baf683cc9cd98ee75811d95636f5480d6eacc3
yowchun93 <yowchun93@gmail.com>
2020-10-14 15:40:54 +0800

    Roman to Arabic

--- roman/roman.go
+++ roman/roman.go
@@ -59,7 +59,9 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+type RomanNumerals []RomanNumeral
+
+var allRomanNumerals = RomanNumerals{
 	{10, "X"},
 	{9, "IX"},
 	{5, "V"},
@@ -77,4 +79,56 @@ func ConvertToRoman(arabic int) string {
 		}
 	}
 	return result.String()
+}
+
+// Only works for I, II
+// func ConvertToArabic(roman string) int {
+// 	arabic := 0
+// 	for range roman {
+// 		arabic++
+// 	}
+// 	return arabic
+// }
+
+// Value should take in bytes instead of string
+// func (r RomanNumerals) ValueOf(symbol string) int {
+// 	for _, s := range r {
+// 		if s.Symbol == symbol {
+// 			return s.Value
+// 		}
+// 	}
+
+// 	return 0
+// }
+
+func (r RomanNumerals) ValueOf(symbols ...byte) int {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
+func ConvertToArabic(roman string) int {
+	total := 0
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if i+1 < len(roman) && symbol == 'I' {
+			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
+				total += value
+				i++
+			} else {
+				total++
+			}
+		} else {
+			value := allRomanNumerals.ValueOf(symbol)
+			total += value
+		}
+	}
+	return total
 }
--- roman/roman_test.go
+++ roman/roman_test.go
@@ -1,6 +1,9 @@
 package roman
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // func TestRomanNumerals(t *testing.T) {
 // 	got := ConvertToRoman(1)
@@ -20,32 +23,75 @@ import "testing"
 // 	})
 // }
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
-	}{
-		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV", 4, "IV"},
-		{"5 gets converted to V", 5, "V"},
-		{"6 gets converted to VI", 6, "VI"},
-		{"9 gets converted to VI", 9, "IX"},
-		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to XIV", 14, "XIV"},
-		{"18 gets converted to XVIII", 18, "XVIII"},
-		{"20 gets converted to XX", 20, "XX"},
-		{"39 gets converted to XXXIX", 39, "XXXIX"},
-	}
+var cases = []struct {
+	Description string
+	Arabic      int
+	Roman       string
+}{
+	{"1 gets converted to I", 1, "I"},
+	{"2 gets converted to II", 2, "II"},
+	{"3 gets converted to III", 3, "III"},
+	{"4 gets converted to IV", 4, "IV"},
+	{"5 gets converted to V", 5, "V"},
+	{"6 gets converted to VI", 6, "VI"},
+	{"9 gets converted to VI", 9, "IX"},
+	{"10 gets converted to X", 10, "X"},
+	{"14 gets converted to XIV", 14, "XIV"},
+	{"18 gets converted to XVIII", 18, "XVIII"},
+	{"20 gets converted to XX", 20, "XX"},
+	{"39 gets converted to XXXIX", 39, "XXXIX"},
+}
 
+func TestRomanNumerals(t *testing.T) {
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got %q, want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
 			}
 		})
 	}
 }
+
+var newCases = []struct {
+	Description string
+	Arabic      int
+	Roman       string
+}{
+	{"1 gets converted to I", 1, "I"},
+	{"2 gets converted to II", 2, "II"},
+	{"4 gets converted to IV", 4, "IV"},
+	{"5 gets converted to V", 5, "V"},
+	{"9 gets converted to VI", 9, "IX"},
+	{"10 gets converted to X", 10, "X"},
+	{"14 gets converted to XIV", 14, "XIV"},
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range newCases[:7] {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
+			}
+		})
+	}
+}
+
+// func TestRomanNumeralValues(t *testing.T) {
+// 	want := 5
+// 	got := allRomanNumerals.ValueOf("V")
+
+// 	if got != want {
+// 		t.Errorf("got %d, want %d", got, want)
+// 	}
+
+// 	t.Run("test IV", func(t *testing.T) {
+// 		want := 4
+// 		got := allRomanNumerals.ValueOf("IV")
+
+// 		if got != want {
+// 			t.Errorf("got %d, want %d", got, want)
+// 		}
+// 	})
+// }
